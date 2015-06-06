200b9ab8751bd2e7d61ccfb06dd47f64514ae92c
powerboat9 <powerboat9@users.noreply.github.com>
2015-06-05 22:42:25 -0400

    Update send.lua

--- apis/ARPA-Reborn/client/send.lua
+++ apis/ARPA-Reborn/client/send.lua
@@ -1,1 +1,14 @@
-function createPacket(data, size
+function createPacket(data, length)
+        local start = 1
+        local i = 1
+        local packet = {}
+        while true do
+                packet[i] = string.sub(data, start, start + (length - 1)
+                start = start + length
+        end
+        return packet
+end
+
+for i = 1, text in pairs(createPacket("qwertyuiopasdfghjklzxcvbnm", 2)) do
+        print(text)
+end
