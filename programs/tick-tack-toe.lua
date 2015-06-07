259918c8d6e24149b7fa38285f4c1a652688a809
powerboat9 <powerboat9@users.noreply.github.com>
2015-06-07 11:27:26 -0400

    Update tick-tack-toe.lua

--- programs/tick-tack-toe.lua
+++ programs/tick-tack-toe.lua
@@ -1,8 +1,16 @@
 local x, y = term.getSize()
+local thirdX, thirdY = x / 3, y / 3
+local two-thirdX, two-thirdY = thirdX * 2, thirdY * 2
+
 if term.isColor() then
-        paintutils.drawBox(0, 0, x / 3, y / 3, color.black)
-        paintutils.drawBox(x / 3, 0, x * 2 / 3, y / 3, color.black)
-        paintutils.drawBox(x * 2 / 3, 0, x, y / 3, color.black)
-        paintutils.drawBox(0,
+        paintutils.drawBox(0, 0, thirdX, thirdY, color.black)
+        paintutils.drawBox(thirdX, 0, two-thirdX, thirdY, color.black)
+        paintutils.drawBox(two-thirdX, 0, x, thirdY, color.black)
+        
+        paintutils.drawBox(0, thirdY, thirdX, two-thirdY, color.black)
+        paintutils.drawBox(thirdX, thirdY, two-thirdX, two-thirdY, color.black)
+        paintutils.drawBox(two-thirdX, thirdY, x, two-thirdY)
+        
+        paintutils.drawBox(0, two-thirdY, thirdX, y, color.black)
         term.setBackgroundColor(color.white)
 
