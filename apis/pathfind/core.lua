0e9f46d6d05ce029ed70f51b578f584881bc4fdc
powerboat9 <powerboat9@users.noreply.github.com>
2015-07-01 16:28:48 -0400

    Update core.lua

--- apis/pathfind/core.lua
+++ apis/pathfind/core.lua
@@ -2,4 +2,17 @@ function nextMove(map, start, end, canMove){
         local mapLayout = {}
         local sizeX = map["sizeX"]
         local sizeY = map["sizeY"]
+        local sizeZ = map["sizeZ"]
+        
+        local size = {}
+        size["sizeX"] = sizeX
+        size["sizeY"] = sizeY
+        size["sizeZ"] = sizeZ
+}
+
+function defaultMove(pos, size, properties){
+        if properties["id"] == 0 then
+                return true
+        end
+        return false
 }
