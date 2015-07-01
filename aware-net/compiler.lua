37cb5a5394c931719bd2d8ea342a171e5ef3ebb6
powerboat9 <powerboat9@users.noreply.github.com>
2015-07-01 12:44:07 -0400

    Update compiler.lua

--- aware-net/compiler.lua
+++ aware-net/compiler.lua
@@ -1,5 +1,9 @@
-local TArgs = {...}
+local tArgs = {...}
 
 function getProperty(text, prop){
         return text.split(prop)[2].split(";")[1]
+}
+
+function getPropertyText(text){
+        return text.split("--[[
 }
