b6011ee8e487ac1081bbb818bfaa86c2bb6fd62b
powerboat9 <powerboat9@users.noreply.github.com>
2015-07-01 12:48:37 -0400

    Update compiler.lua

--- aware-net/compiler.lua
+++ aware-net/compiler.lua
@@ -4,6 +4,6 @@ function getProperty(text, prop){
         return text.split(prop)[2].split(";")[1]
 }
 
-function getPropertyText(text){
-        return text.split("--[[
+function getPropertyText(section, text){
+        return text.split("--[[<" + section + ">")[2].split("END]]--").
 }
