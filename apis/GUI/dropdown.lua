10d73673d8629b4537107aab2de1e808386e1059
powerboat9 <powerboat9@users.noreply.github.com>
2015-07-02 22:21:10 -0400

    Update dropdown.lua

--- apis/GUI/dropdown.lua
+++ apis/GUI/dropdown.lua
@@ -1,1 +1,8 @@
-
+function change(startX, startY, endX, endY, screen, color, alpha)
+    local tempScreen = screen
+    for i=startY, endY do
+        for j=startX, endX do
+            tempScreen[j][k] = screen[j][k]
+        end
+    end
+end
