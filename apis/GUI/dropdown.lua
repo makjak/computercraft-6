fc3389b32c7e082cc305ea0d07e21e285d9ddd41
powerboat9 <powerboat9@users.noreply.github.com>
2015-07-03 10:13:14 -0400

    Update dropdown.lua

--- apis/GUI/dropdown.lua
+++ apis/GUI/dropdown.lua
@@ -2,7 +2,7 @@ function change(startX, startY, endX, endY, screen, color, alpha)
     local tempScreen = screen
     for i=startY, endY do
         for j=startX, endX do
-            tempScreen[j][k] = screen[j][k]
+            tempScreen[j][k] = screen[j][k] * (100 - alpha) + color * alpha
         end
     end
 end
