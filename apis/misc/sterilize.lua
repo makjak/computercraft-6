b6807e49e2ba59404b6b30101c7af8ec5d5ddaaa
powerboat9 <powerboat9@users.noreply.github.com>
2015-07-08 15:46:31 -0400

    Update sterilize.lua

--- apis/misc/sterilize.lua
+++ apis/misc/sterilize.lua
@@ -1,3 +1,8 @@
-function fromTable(table)
-    
+function fromTableRaw(table)
+    local txt = ""
+    for k, v in pairs(table) do
+        key = base64(k)
+        value = base64(v)
+        txt = txt .. key .. ":" .. value .. "\n"
+    end
 end
