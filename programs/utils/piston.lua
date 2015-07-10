d6ddb596135b1757ace400b9396c5bfa2b168794
powerboat9 <powerboat9@users.noreply.github.com>
2015-07-10 15:27:37 -0400

    Update piston.lua

--- programs/utils/piston.lua
+++ programs/utils/piston.lua
@@ -4,7 +4,8 @@ local package = tArgs[2]
 local installPath = ""
 if usr = %ROOT% then
     installPath = "/usr/pkgs" .. package
-elseif 
+elseif usr ~= string.match(usr, "[%w_-]*") or fs.isDir("/home/" .. usr) == false
+    error("User contains invalid characters")
 else
     installPath = "/home/" .. usr .. "/bin/pkgs"
 end
