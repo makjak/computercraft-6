1462d0176d08caa1ee44f9681d59f330a804ed23
powerboat9 <powerboat9@users.noreply.github.com>
2015-07-10 14:45:35 -0400

    Update piston.lua

--- programs/utils/piston.lua
+++ programs/utils/piston.lua
@@ -1,1 +1,10 @@
-
+tArgs = {...}
+local usr = tArgs[1]
+local package = tArgs[2]
+local installPath = ""
+if usr = %ROOT% then
+    installPath = "/usr/pkgs" .. package
+elseif 
+else
+    installPath = "/home/" .. usr .. "/bin/pkgs"
+end
