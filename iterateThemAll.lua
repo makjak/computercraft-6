ae7a40d7c7479165a378713b222bb35fd61b1fc0
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-04 09:06:28 -0400

    Update iterateThemAll.lua

--- iterateThemAll.lua
+++ iterateThemAll.lua
@@ -1,9 +1,18 @@
 local data = {}
+local finish = {}
 
 for k, v in pairs(
 
 for k, v in pairs(data) do
     if tostring(v) == tostring(_G) or tostring(v) == tostring(data) then
         table.remove(data, 1)
+        continue
+    elseif type(v) == "table" then
+        for kk, vv in pairs(v) do
+            finish[k .. "________" .. kk] = vv
+        end
+        table.remove(data, 1)
     else
+        finish[k] = v
+    end
 end
