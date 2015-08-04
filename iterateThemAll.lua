51edb70dc6429eb567cf6fab55f10646c0c11647
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-04 08:58:22 -0400

    Update iterateThemAll.lua

--- iterateThemAll.lua
+++ iterateThemAll.lua
@@ -4,6 +4,6 @@ for k, v in pairs(
 
 for k, v in pairs(data) do
     if tostring(v) == tostring(_G) or tostring(v) == tostring(data) then
-        table.remove(data
-    end
+        table.remove(data, 1)
+    else
 end
