c45e01070c275b7dd195c2805cb627643d64f307
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-05 13:38:43 -0400

    Update physics.lua

--- apis/BioLib/physics.lua
+++ apis/BioLib/physics.lua
@@ -1,7 +1,8 @@
 local worldMeta = {
     function passSecond(self)
         for k, v in ipairs(self.points) do
-            pos = self.points[k].pos
-            x, y, z = unpack(pos)
+            local pos = self.points[k].pos
+            local x, y, z = unpack(pos)
+            local motion = self.points
         end
     end
