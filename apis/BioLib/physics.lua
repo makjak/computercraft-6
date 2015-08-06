6ecec0e7f4f79ca971c74a1f060aa1d76a245cd7
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-05 21:06:46 -0400

    Update physics.lua

--- apis/BioLib/physics.lua
+++ apis/BioLib/physics.lua
@@ -3,12 +3,12 @@ local worldMeta = {
         local logReturn = ""
         for k, v in ipairs(self.points) do
             self:updateMotion()
-            local pos = self.points.[k].pos
+            local pos = self.points[k].pos
             local x, y, z = unpack(pos)
-            local motion = self.points.[k].motion
+            local motion = self.points[k].motion
             local motionX, motionY, motionZ = unpack(motion)
             local newX, newY, newZ = motionX + x, motionY + y, motionZ + z
-            logReturn = logReturn + format("Moved %s
+            logReturn = logReturn + format("Moved %s from %d, %d, %d to %d, %d, %d at %s", self.points[k].name)
         end
     end
 end
