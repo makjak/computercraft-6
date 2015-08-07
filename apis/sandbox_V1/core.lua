6a57ff62598b1ac2c0f19cd32fe1803c62d58eb3
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-06 20:44:00 -0400

    Create core.lua

---
+++ apis/sandbox_V1/core.lua
@@ -0,0 +1,9 @@
+local denyFiles = {}
+
+local allowed = function(t, action, k)
+    return denyFiles[tostring(t)][action][k]
+end
+
+function deny(t, action, k)
+    print("For " .. tostring(t) .. ", " .. action .. "ing index " .. k .. "is not allowed")
+end
