1b7036609be0675e9edb560bf4d3418608941567
powerboat9 <powerboat9.gamer@gmail.com>
2015-08-09 21:34:36 -0400

    Work on parsing

--- programs/utils/OS/ONIMU/JSON_parse.lua
+++ programs/utils/OS/ONIMU/JSON_parse.lua
@@ -1,8 +1,15 @@
+function removeIf(txt, i, match)
+    return string.sub(txt, 1, i - 1) .. ((string.sub(txt, i, i) == match) ? "" : string.sub(txt, i, i)) .. string.sub(txt, i + 1)
+end
+
 function parseLayer(txt)
-    local parsed = string.sub(txt, (string.sub(txt, 1, 1) == "{") ? 2 : 1, string.len(txt) - ((string.sub(txt, string.len(txt) - 1)) ? 1 : 0))
+    local parsed = string.sub(txt, (string.sub(txt, 1, 1) == "{") ? 2 : 1, string.len(txt) - ((string.sub(txt, string.len(txt) - 1) == "}") ? 1 : 0))
 	return parsed
 end
 
 --print(parseLayer("{Stuff:\"TXT\"}"))
 
-print(tostring(#txt))
+--fun = "fun"
+--print(fun[1])
+
+print(removeIf("Fun", 1, "F"))
