bb8d6983d6b48ebf3fdbca5b5325d8804b8921cd
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-11 13:55:30 -0400

    Update core.lua

--- apis/Learn_v1/core.lua
+++ apis/Learn_v1/core.lua
@@ -2,8 +2,22 @@ local actionMeta = {
     fire = {},
     
     pointer = 1,
-    setPointer = function(self)
-        self.pointer = 1
+    
+    setPointer = function(self, newPointer)
+        self.pointer = newPointer
+    end,
+    
+    resetPointer = function(self)
+        self:setPointer(1)
+    end,
+    
+    writeData = function(self, nodes)
+        self.fire[self.pointer] = nodes
     end
-    writeData = function(self)
-        
+}
+
+function newAction()
+    local action = {}
+    setmetatable(action, actionMeta)
+    return action
+end
