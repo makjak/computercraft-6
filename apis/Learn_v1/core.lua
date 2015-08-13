4e382d937ade15fca6e0de5ea513f803429c8492
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-13 11:36:24 -0400

    Update core.lua

--- apis/Learn_v1/core.lua
+++ apis/Learn_v1/core.lua
@@ -1,10 +1,57 @@
+function findInsertSpot(t)
+    lastK = 0
+    for k, v in pairs(t) do
+        if lastK + 1 < k then
+            return lastK + 1
+        elseif v == nil then
+            return k
+        else
+            lastK++
+        end
+    end
+end
+
 local nodeMeta = {
-    nodeInputs = {},
-    nodeOutputs = {},
+    name = "(Unregistered)"
     
-    findInsertSpot = function(t)
-        for k, v in pairs(t) do
-            
+    binary = true
+    value = 0
+    
+    unstable = 0
+    ticksStable = 0
+    
+    hold = 0
+    holdValue = 0
+    
+    setValue = function(self, reason, params)
+        reason = reason or "Unknown"
+        
+        if reason == "EMP" then
+            self.unstable = 1
+            self.ticksStable = params["ticks"]
+        elseif reason == "Hold"
+            self.unstable = 0
+            self.ticksStable = 0
+            self.holdValue = params["newValue"]
+        elseif reason == "Unknown"
+            error("You tried to call setValue() on " .. self.name .. "with no valid reason. You may see unexpected results")
+        else
+            self.value = params["newValue"]
+        end
+    end
+}
+
+local nodeNetMeta = {
+    nodeDictionary = {},
+    nodes = {},
+    
+    newNode
+    
+    addNode = function(self, name)
+        if nodes[name] == nil then
+            local newNode = {}
+            set
+            nodes[name] = 
 }
 
 local actionMeta = {
