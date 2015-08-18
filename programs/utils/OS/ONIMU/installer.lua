844253d0524bb1511e63d57a24675736bacdd6a5
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-17 22:00:26 -0400

    Update installer.lua

--- programs/utils/OS/ONIMU/installer.lua
+++ programs/utils/OS/ONIMU/installer.lua
@@ -2,7 +2,7 @@ term.clear()
 term.setCursorPos(0, 0)
 
 function copyInternet(http, file)
-    if type("http") == "string" then
+    if type("http") != "string" then
         error(2, "Expected String For URL")
     elseif
     file = shell.resolve(file)
