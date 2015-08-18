9f1f38f688343e455e75facc6a801291e125e230
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-17 21:59:15 -0400

    Rename installer.lua to installer2.lua

---
+++ programs/utils/OS/ONIMU/installer2.lua
@@ -0,0 +1,8 @@
+term.clear()
+term.setCursorPos(0, 0)
+
+function copyInternet(http, file)
+    if type("http") == "string" then
+        error(2, "Expected String For URL")
+    elseif
+    file = shell.resolve(file)
